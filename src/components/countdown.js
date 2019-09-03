f029bb7622fa514387c01dd95a86f47c7993df71
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-09-03 10:39:25 +0200

    node modules neu

--- src/components/countdown.js
+++ src/components/countdown.js
@@ -1,22 +1,27 @@
-import React, { Component } from "react";
-import Img from "gatsby-image"
-import { Link } from "gatsby"
+import React, { Component } from 'react'
+import Img from 'gatsby-image'
+import { Link } from 'gatsby'
 
 export default class Countdown extends React.Component {
   render() {
-    const { data } = this.props;
+    const { data } = this.props
     return (
       <div className="countdown-section">
         <Img sizes={data.featureImage.fluid} />
         <div className="container">
           <div className="countdown-inner">
             <h2 className="with-underline">{data.title}</h2>
-            <span className="date">Last Date :</span>
-            <span className="date"><strong><i className="fas fa-clock"></i>{data.date}</strong></span>
-            <Link to="/store">Shop Now</Link>
+            <span className="date">Letzter Eintrag :</span>
+            <span className="date">
+              <strong>
+                <i className="fas fa-clock"></i>
+                {data.date}
+              </strong>
+            </span>
+            <Link to="/store">Jetzt einkaufen</Link>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }

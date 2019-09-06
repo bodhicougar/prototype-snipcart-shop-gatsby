7cb6857579b15a9a895ddc6deab249a245254e7e
bodhicougar <bodhicougar@bodhicougar@a1.net>
2019-09-06 13:09:11 +0200

    Eslint richtiggestellt

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,13 +5,13 @@
  * See: https://www.gatsbyjs.org/docs/static-query/
  */
 
-import React from "react"
-import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import React from 'react'
+import PropTypes from 'prop-types'
+import { StaticQuery, graphql } from 'gatsby'
 
-import Header from "./header"
-import Footer from "./footer"
-import "./layout.css"
+import Header from './header'
+import Footer from './footer'
+import './layout.css'
 
 const Layout = ({ children }) => (
   <StaticQuery
